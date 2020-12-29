32a75606793798a1775f0043bc992f0c7f2cfb98
Mayron Sousa <mayron.hen@gmail.com>
2020-12-29 16:54:01 -0300

    first commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import *
 
 app = Flask(__name__) #Criando um Objeto da classe Flask
 
@@ -20,8 +20,32 @@ def home(age):
 def about():
 	return "Sobre: "
 
-
+# Função para criar urls no lugar dos decorators
 app.add_url_rule("/about", "about", about)
+
+@app.route('/admin')
+def admin():
+    return 'admin'
+
+
+@app.route('/librarion')
+def librarion():
+    return 'librarion'
+
+
+@app.route('/student')
+def student():
+    return 'student'
+
+
+@app.route('/user/<name>')
+def user(name):
+    if name == 'admin':
+        return redirect(url_for('admin'))
+    if name == 'librarion':
+        return redirect(url_for('librarion'))
+    if name == 'student':
+        return redirect(url_for('student'))
 
 if __name__ == "__main__": # Executa app.run() quando chamar python app.py
 	app.run(debug=True)
