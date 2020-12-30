2c4013aba8ab347b597a118d35ff69115cbedf53
Mayron Sousa <mayron.hen@gmail.com>
2020-12-30 10:05:40 -0300

    some more examples

---
+++ Readme.md
@@ -0,0 +1,11 @@
+# Flask Basics 
+
+Some basics examples and  reminders for developing with [Flask](https://flask.palletsprojects.com/en/1.1.x/).
+
+## Getting Started
+
+In this project I use [Pipenv](https://github.com/pypa/pipenv), so after cloning the repo just run the command to install the dependencies for Flask
+
+```
+	pipenv install
+```
--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import *
 app = Flask(__name__) #Criando um Objeto da classe Flask
 
 @app.route('/') #decorator
-def home():
+def index():
 	return "Hello World!"
 
 
@@ -12,8 +12,8 @@ def home(name):
 	return "Hello, " + name
 
 
-@app.route('/home/<int:age>')
-def home(age):
+@app.route('/age/<int:age>')
+def age(age):
 	return "Idade: %d"%age
 
 
@@ -41,11 +41,38 @@ def student():
 @app.route('/user/<name>')
 def user(name):
     if name == 'admin':
+        # Função para redirecionar para a função com o nome admin
         return redirect(url_for('admin'))
     if name == 'librarion':
         return redirect(url_for('librarion'))
     if name == 'student':
         return redirect(url_for('student'))
+
+
+@app.route('/hello/<user>')
+def message(user):
+    # Passar um objeto para o template
+    return render_template('message.html', name=user)
+
+
+@app.route('/table/<int:num>')
+def table(num):
+    return render_template('tableOf.html', n=num)
+
+
+@app.route('/signup')
+def signUpForm():
+    return render_template('singup.html')
+
+
+# Cria 2 rotas success, uma para POST e outra para GET
+@app.route('/success', methods=['POST', 'GET'])
+def printData():
+    if request.method == 'POST':
+        # O .form é um dicionário que contem um par de chave-valor vindo dos parametros do form
+        result = request.form
+        return render_template("showData.html", result=result)
+
 
 if __name__ == "__main__": # Executa app.run() quando chamar python app.py
 	app.run(debug=True)
